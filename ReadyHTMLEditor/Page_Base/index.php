75def5dc6273e390153f22ddde05c7d7b2b83f13
Gerard KESSE <tiakagerard@hotmail.com>
2018-03-14 17:49:27 +0100

    Initial Commit

--- ReadyHTMLEditor/Page_Base/index.php
+++ ReadyHTMLEditor/Page_Base/index.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="utf-8" />
 		<title>ReadyHTMLEditor</title>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="style.css" />
 		<script src="editeur.js"></script>
 	</head>
@@ -10,7 +11,8 @@
 		<input type="button" value="G" style="font-weight:bold;" onclick="commande('bold');" />
 	    <input type="button" value="I" style="font-style:italic;" onclick="commande('italic');" /> 
 	    <input type="button" value="S" style="text-decoration:underline;" onclick="commande('underline');" /> 
-	    <input type="button" value="abc" style="text-decoration:line-through;" onclick="commande('line-through');" /> 
+	    <input type="button" value="abc" style="text-decoration:line-through;" onclick="commande('strikeThrough');" /> 
+	    <button type="button" onclick="commande('createLink');"><i class="fa fa-link"></i></button>		
 	    <div id="editeur" contentEditable ></div> 
 		<div>
 	</body>
