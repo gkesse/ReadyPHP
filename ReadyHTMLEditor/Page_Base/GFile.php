acb2e2849b8e26bd2c514e6593eeb8bc701f6913
Gerard KESSE <tiakagerard@hotmail.com>
2018-03-15 12:10:24 +0100

    Initial Commit

---
+++ ReadyHTMLEditor/Page_Base/GFile.php
@@ -0,0 +1,46 @@
+<?php   
+    class GFile {
+        //===============================================
+        private static $m_instance = null;
+        //===============================================
+        private function __construct() {
+        
+        }
+        //===============================================
+        public static function Instance() {
+            if(is_null(self::$m_instance)) {
+                self::$m_instance = new GFile();  
+            }
+            return self::$m_instance;
+        }
+        //===============================================
+        public function getData($file) {
+            if($file[0] != "/") $file = "/".$file;
+            $m_file = $_SERVER["DOCUMENT_ROOT"].$file;
+            $m_data = file_get_contents($m_file);
+            return $m_data;
+        }
+        //===============================================
+        public function getData2($file) {
+            $m_data = file_get_contents($file);
+            return $m_data;
+        }
+        //===============================================
+        public function getInclude($file, $data) {
+            ob_start();                    
+            include($file);  
+            $m_content = ob_get_contents();
+            ob_end_clean();
+            return $m_content;
+        }
+        //===============================================
+        public function getDate($file) {
+            if($file[0] != "/") $file = "/".$file;
+            $m_file = $_SERVER["DOCUMENT_ROOT"].$file."index.php";
+            $m_date = filemtime($m_file);
+            $m_date = date ("Y-m-d", $m_date);
+            return $m_date;
+        }
+        //===============================================
+    }
+?>
--- ReadyHTMLEditor/Page_Base/index.php
+++ ReadyHTMLEditor/Page_Base/index.php
@@ -1,13 +1,19 @@
+<?php
+    require "GAutoloadRegister.php";
+    
+    $m_data = GJson::Instance()->getData("PageEditor.json");
+?>
+<!-- ============================================ -->
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8" />
 		<title>ReadyHTMLEditor</title>
-                <!-- ============================================ -->
-        <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Aclonica"/>
-        <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Akronim"/>
-        <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Allan"/>
-        <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Archivo Narrow"/>
+		<!-- ============================================ -->
+		<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Aclonica"/>
+		<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Akronim"/>
+		<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Allan"/>
+		<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Archivo Narrow"/>
 		<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Anton"/>
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 		<link rel="stylesheet" href="style.css" />
@@ -16,16 +22,41 @@
 	<body>
 		<!-- ============================================ -->
 		<div class="pgBg">
-			<div class="mrga pgBd ftfe ftsg">
+			<div class="mrga pgBd ftfe ftsg pdtb">
+				<!-- ============================================ -->
+				<div class="pgCt10">
+					<div class="brda">
+						<h1 class="bgra pgCt10" id="Sommaire">
+							<a class="clrb" href="#">
+								Sommaire
+							</a>
+						</h1>
+						<div class="txal pgCt10">
+							<ul class="fa-ul">
+								<?php 
+                            $m_ds = $m_data["summary"];
+                            foreach($m_ds as $m_di) {
+                            ?>
+								<li>
+									<i class="fa-li fa fa-book clrg"></i>
+									<a class="hvra clrg" href="<?php echo $m_di["link"]; ?>">
+										<?php echo $m_di["name"]; ?>
+									</a>
+								</li>
+								<?php } ?>
+							</ul>            
+						</div>
+					</div>
+				</div>
 				<!-- ============================================ -->
 				<div class="pgCt10 txal">
-					<div class="">
-						<!-- ============================================ -->
-						<div class="bgra pddc fts0">
+					<!-- ============================================ -->
+					<div class="psfa">
+						<div class="bgrh pddc fts0 txac">
 							<button class="evta pddc ftsb ftwb" type="button" title="Texte en Gras" onclick="commande('bold');">G</button>
 							<button class="evta pddc ftsb ftti" type="button" title="Texte en Italic" onclick="commande('italic');">I</button>
 							<button class="evta pddc ftsb txdu" type="button" title="Souligner Texte" onclick="commande('underline');">S</button> 
-							<button class="evta pddc ftsb" type="button" title="Barrer Texte" onclick="commande('strikeThrough');">abc</button>
+							<button class="evta pddc ftsb txdl" type="button" title="Barrer Texte" onclick="commande('strikeThrough');">abc</button>
 							<button class="evta pddc ftsb" type="button" title="Insérer Lien" onclick="commande('createLink');"><i class="fa fa-link"></i></button>		
 							<button class="evta pddc ftsb" type="button" title="Supprimer Lien" onclick="commande('unlink');"><i class="fa fa-unlink"></i></button>		
 							<button class="evta pddc ftsb" type="button" title="Insérer Image" onclick="commande('insertImage');"><i class="fa fa-image"></i></button>		
@@ -34,8 +65,8 @@
 							<button class="evta pddc ftsb" type="button" title="Text au Centre" onclick="commande('justifycenter');"><i class="fa fa-align-center"></i></button>		
 							<button class="evta pddc ftsb" type="button" title="Texte à Droite" onclick="commande('justifyright');"><i class="fa fa-align-right"></i></button>		
 							<button class="evta pddc ftsb" type="button" title="Texte Justifié" onclick="commande('justifyfull');"><i class="fa fa-align-justify"></i></button>		
-							<button class="evta pddc ftsb" type="button" title="Puces" onclick="commande('insertunorderedlist');"><i class="fa fa-image"></i></button>		
-							<button class="evta pddc ftsb" type="button" title="Puces Ordonnées" onclick="commande('insertorderedlist');"><i class="fa fa-image"></i></button>		
+							<button class="evta pddc ftsb" type="button" title="Puces" onclick="commande('insertunorderedlist');"><i class="fa fa-list"></i></button>		
+							<button class="evta pddc ftsb" type="button" title="Puces Ordonnées" onclick="commande('insertorderedlist');"><i class="fa fa-list-ol"></i></button>		
 							<button class="evta pddc ftsb" type="button" title="Souligné" onclick="commande('justifyfull');"><i class="fa fa-image"></i></button>		
 							<button class="evta pddc ftsb" type="button" title="Souligné" onclick="commande('justifyfull');"><i class="fa fa-image"></i></button>		
 							<button class="evta pddc ftsb" type="button" title="Souligné" onclick="commande('justifyfull');"><i class="fa fa-image"></i></button>		
@@ -50,19 +81,21 @@
 								<option value="h6">Titre 6</option>
 							</select>
 						</div>
-						<div class="brda mnha pgCt10" id="codeEditorId" contentEditable></div> 
-						<!-- ============================================ -->
-						<div class="pddi">
-							<button class="evta" type="button" onclick="codeView();">Aperçu</button>
-						</div>
-						<div class="mnha pgCt10" id="codeViewId"></div>
-						<!-- ============================================ -->
-						<!--div class="pddi">
+					</div>
+					<div class="brda hghd ovfa">
+						<div class="pgCt10" id="codeEditorId" contentEditable></div> 
+					</div>
+					<!-- ============================================ -->
+					<div class="pddi">
+						<button class="evta" type="button" onclick="codeView();">Aperçu</button>
+					</div>
+					<div class="mnha pgCt10" id="codeViewId"></div>
+					<!-- ============================================ -->
+					<!--div class="pddi">
 							<button class="evta" type="button" onclick="codeHtml();">Obtenir le HTML</button>
 						</div>
 						<textarea class="bgra mnha pgCt10" id="codeHtmlId"></textarea-->
-						<!-- ============================================ -->
-					</div>
+					<!-- ============================================ -->
 				</div>
 				<!-- ============================================ -->
 			</div>
