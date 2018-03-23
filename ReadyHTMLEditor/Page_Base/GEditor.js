34906c76795af12fe908acfe95e0f69bff34246b
Gerard KESSE <tiakagerard@hotmail.com>
2018-03-23 13:01:27 +0100

    Initial Commit

--- ReadyHTMLEditor/Page_Base/GEditor.js
+++ ReadyHTMLEditor/Page_Base/GEditor.js
@@ -31,10 +31,22 @@ var GEditor = (function() {
 				var m_selection = document.getSelection();
 				var m_startNode = m_selection.anchorNode;
 				var m_data = m_startNode.data;
+				var m_length = m_data.length;
+				var m_range = document.createRange();
+				var m_parentNode = m_startNode.parentNode.parentNode;
 				var m_command = '';
-				
+																
 				switch(arg) {
 				case 'h1':
+					if(m_parentNode.nodeName == "H1") {
+						m_range.selectNode(m_parentNode);
+						m_selection.addRange(m_range);
+						document.execCommand("insertHTML", false, m_data);
+						break;
+					}
+					m_range.setStart(m_startNode, 0);
+					m_range.setEnd(m_startNode, m_length);
+					m_selection.addRange(m_range);
 					m_command += '<h1 class="bgra pgCt20" id="Sommaire">';
 					m_command += '<a class="clrb" href="#">';
 					m_command += m_data;
