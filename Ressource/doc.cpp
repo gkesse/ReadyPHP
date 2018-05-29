2f070f9206dd6023549756985815e5bcdeebbf97
Gerard KESSE <tiakagerard@hotmail.com>
2018-05-29 14:48:27 +0200

    Initial Commit

--- Ressource/doc.cpp
+++ Ressource/doc.cpp
@@ -1,24 +1,23 @@
 //===============================================
 // WampServer Telechargement
+//===============================================
 http://www.wampserver.com/
 https://sourceforge.net/projects/wampserver/files/
 https://sourceforge.net/projects/wampserver/files/WampServer%202/
 https://sourceforge.net/projects/wampserver/files/WampServer%202/Wampserver%202.5/
+
 //===============================================
 // WampServer Virtual Host Autorisation
+//===============================================
 C:\wamp64\bin\apache\apache2.4.27\conf
 httpd.conf
 Decommenter
 LoadModule vhost_alias_module modules/mod_vhost_alias.so
 Include conf/extra/httpd-vhosts.conf
+
 //===============================================
 // WampServer Virtual Host Configuration
+//===============================================
 C:\wamp64\bin\apache\apache2.4.27\conf\extra
 httpd-vhosts.conf
-//===============================================
-// 
-//===============================================
-// 
-//===============================================
-// 
-//===============================================
+
