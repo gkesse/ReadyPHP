ae232288dfe8307b199ea701a4dec55823c7f388
Gerard KESSE <tiakagerard@hotmail.com>
2018-05-29 15:06:13 +0200

    Initial Commit

---
+++ Ressources/Namespace/GNamespace.php
@@ -0,0 +1,23 @@
+<?php 
+    class GNamespace {
+        //===============================================
+        private static $m_instance = null;
+        //===============================================
+        private function __construct() {
+
+        }
+        //===============================================
+        public static function Instance() {
+            if(is_null(self::$m_instance)) {
+                self::$m_instance = new GNamespace();  
+            }
+            return self::$m_instance;
+        }
+        //===============================================
+        public function run() {
+            $lData = "Bonjour tout le monde";
+            print_r($lData);
+        }
+        //===============================================
+    }
+?>        
---
+++ Ressources/Namespace/index.php
@@ -0,0 +1,4 @@
+<?php 
+    require $_SERVER["DOCUMENT_ROOT"]."/Namespace/GAutoloadRegister.php";
+    GNamespace::Instance()->run();
+?>
