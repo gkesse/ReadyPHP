3c8bfc7d25f7b8ab400caf3a29b1f650007efc68
Gerard KESSE <tiakagerard@hotmail.com>
2021-01-18 21:30:02 +0100

    Initial Commit

---
+++ cmd/win/env.bat
@@ -0,0 +1,18 @@
+@echo off
+::===============================================
+set "PATH=C:\MinGW\bin;%PATH%" 
+::===============================================
+set "GPROJECT_ROOT=C:\Users\Admin\Downloads\Programs"
+set "GPROJECT_PATH=%GPROJECT_ROOT%\ReadyPHP"
+::===============================================
+set "GCOMPOSER_INSTALL_PATH=C:\Users\Admin\Downloads\Programs\ReadyTest\php\p02\libs\phpspreadsheet"
+set "GCOMPOSER_INSTALL_PATH=C:\Users\Admin\Downloads\Programs\ReadyTest\php\p03\libs"
+set "GCOMPOSER_INSTALL_PKG=phpoffice/phpspreadsheet --prefer-source"
+set "GCOMPOSER_INSTALL_PKG=dompdf/dompdf"
+set "GCOMPOSER_INSTALL_PKG=amenadiel/jpgraph:^4"
+::===============================================
+set "GPHP_SERVER_PATH=C:\Users\Admin\Downloads\Programs\ReadyPHP\webroot"
+::===============================================
+set "GGIT_URL=https://github.com/gkesse/ReadyPHP.git"
+set "GGIT_NAME=ReadyPHP"
+::===============================================
