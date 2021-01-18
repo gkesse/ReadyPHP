bc76043b4f2e99517e1259e764bd8e0d4258d1b4
Gerard KESSE <tiakagerard@hotmail.com>
2021-01-19 00:02:20 +0100

    Initial Commit

--- cmd/win/env.bat
+++ cmd/win/env.bat
@@ -7,12 +7,12 @@ set "GPROJECT_PATH=%GPROJECT_ROOT%\ReadyPHP"
 ::===============================================
 set "GCOMPOSER_INSTALL_PATH=C:\Users\Admin\Downloads\Programs\ReadyTest\php\p02\libs\phpspreadsheet"
 set "GCOMPOSER_INSTALL_PATH=C:\Users\Admin\Downloads\Programs\ReadyTest\php\p03\libs"
-set "GCOMPOSER_INSTALL_PATH=C:\Users\Admin\Downloads\Programs\ReadyPHP\webroot\libs"
+set "GCOMPOSER_INSTALL_PATH=C:\Users\Admin\Downloads\Programs\ReadyPHP\webroot\libs\composer"
 ::
-set "GCOMPOSER_INSTALL_PKG=phpoffice/phpspreadsheet"
-set "GCOMPOSER_INSTALL_PKG=dompdf/dompdf"
 set "GCOMPOSER_INSTALL_PKG=amenadiel/jpgraph:^4"
 set "GCOMPOSER_INSTALL_PKG=symfony/filesystem:^5.2"
+set "GCOMPOSER_INSTALL_PKG=dompdf/dompdf:^1.0"
+set "GCOMPOSER_INSTALL_PKG=phpoffice/phpspreadsheet:^1.16"
 ::===============================================
 set "GPHP_SERVER_PATH=C:\Users\Admin\Downloads\Programs\ReadyTest\php\p02\libs\phpspreadsheet\vendor\phpoffice\phpspreadsheet\samples"
 ::===============================================
